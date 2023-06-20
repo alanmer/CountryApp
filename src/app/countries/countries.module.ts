3085676ecd29c2b077bf3996942e55d30213c2bf
ALANMer <alanmercadoamg@gmail.com>
2023-06-20 09:41:29 -0600

    Cambios  por id

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -7,11 +7,13 @@ import { CountriesRoutingModule } from './countries-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { CountryTableComponent } from './components/country-table/country-table.component';
 import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
 
 @NgModule({
   imports: [CommonModule, CountriesRoutingModule, SharedModule],
   declarations: [
     ByCapitalPageComponent,
+    CountryPageComponent,
     ByRegionPageComponent,
     CountryTableComponent,
     ByCountryPageComponent,
--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,13 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { count, switchMap } from 'rxjs';
 import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
-  styleUrls: ['./country-page.component.css'],
+  styleUrls: [],
 })
 export class CountryPageComponent implements OnInit {
+  public country?: Country;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private countriesService: CountriesService,
@@ -16,16 +19,15 @@ export class CountryPageComponent implements OnInit {
   ) {}
   ngOnInit(): void {
     this.activatedRoute.params
-    .pipe(switchMap(({id})=>this.countriesService.searchCountryByAlphaCode(id))
-    ).subscribe(country => {
-      if (!country) {
-        return this.router.navigateByUrl('');
+      .pipe(
+        switchMap(({ id }) =>
+          this.countriesService.searchCountryByAlphaCode(id)
+        )
+      )
+      .subscribe(country => {
+        if (!country) return this.router.navigateByUrl('');
+        return this.country = country;
 
-      }
-      console.log("first")
-      return
-    });
-
+      });
   }
-
 }
